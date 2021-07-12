4b3e59d70157c9275e6bc636df15a352eb9e5159
arnaudframmery <arnaud.frammery@gmail.com>
2021-07-12 17:59:45 +0200

    update pypi version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('./README.md', encoding='utf-8') as f:
 
 setup(
     name="pyqt5-tile-layout",
-    version="0.1.5",
+    version="0.1.6",
     author="Arnaud Frammery",
     author_email="arnaud.frammery@gmail.com",
     description="A tile layout for PyQt5",
